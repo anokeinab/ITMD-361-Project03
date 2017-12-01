735693f64c49db43910696025556d8bb2750f462
Divin Gregis Baniekona <31362277+anokeinab@users.noreply.github.com>
2017-12-01 15:11:19 -0600

    Update brazzavillemap.js
    change variable's name from uluru to brazzaville

--- brazzavillemap.js
+++ brazzavillemap.js
@@ -1,11 +1,11 @@
 function initMap() {
-        var uluru = {lat: -4.27934, lng: 15.26039};
+        var brazzaville = {lat: -4.27934, lng: 15.26039};
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 4,
-          center: uluru
+          center: brazzaville
         });
         var marker = new google.maps.Marker({
-          position: uluru,
+          position: brazzaville,
           map: map
         });
       }
